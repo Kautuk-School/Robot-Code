99840fc0f4ffbc3c01b0a513f645e3f374a8fe66
SahilGokhale <169070264+SahilGokhale@users.noreply.github.com>
2024-05-13 11:13:12 -0400

    Update readColorSensor.ino

--- CarRobotProgram/readColorSensor.ino
+++ CarRobotProgram/readColorSensor.ino
@@ -3,15 +3,15 @@ tcs3200 tcs(COLOR_0, COLOR_1, COLOR_2, COLOR_3, COLOR_OUT);
 
 // Function to read color sensor values
 void readColorSensor() {
-  const int samples = 5; // Number of samples to average
-  long sumRed = 0, sumGreen = 0, sumBlue = 0, sumWhite = 0; // Variables to store sum of readings
+  const int samples = 5;                                     // Number of samples to average
+  long sumRed = 0, sumGreen = 0, sumBlue = 0, sumWhite = 0;  // Variables to store sum of readings
 
   // Loop for taking multiple samples and summing up the readings
   for (int i = 0; i < samples; i++) {
-    sumRed += tcs.colorRead('r'); // Read red component
-    sumGreen += tcs.colorRead('g'); // Read green component
-    sumBlue += tcs.colorRead('b'); // Read blue component
-    sumWhite += tcs.colorRead('c'); // Read clear (white) component
+    sumRed += tcs.colorRead('r');    // Read red component
+    sumGreen += tcs.colorRead('g');  // Read green component
+    sumBlue += tcs.colorRead('b');   // Read blue component
+    sumWhite += tcs.colorRead('c');  // Read clear (white) component
   }
 
   // Calculate average readings
