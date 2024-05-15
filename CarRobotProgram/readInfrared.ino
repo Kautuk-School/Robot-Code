46156fb70dd30bb87c3c7e7039cc5f9a18e20ac2
Kautuk-School <169073469+Kautuk-School@users.noreply.github.com>
2024-05-15 11:31:42 -0400

    Update readInfrared.ino

--- CarRobotProgram/readInfrared.ino
+++ CarRobotProgram/readInfrared.ino
@@ -1,18 +1,21 @@
 // Function to read values from infrared sensors
-void readInfrared() {
+int* readInfrared() {
+  static int IRvalues[4];
   // Read values from each infrared sensor
-  int IRvalue1 = digitalRead(IR_1);
-  int IRvalue2 = digitalRead(IR_2);
-  int IRvalue3 = digitalRead(IR_3);
-  int IRvalue4 = digitalRead(IR_4);
+  IRvalues[0] = digitalRead(IR_1);
+  IRvalues[1] = digitalRead(IR_2);
+  IRvalues[2] = digitalRead(IR_3);
+  IRvalues[3] = digitalRead(IR_4);
 
   // Print the values of each infrared sensor
   Serial.print("IR_1: ");
-  Serial.print(IRvalue1);
+  Serial.print(IRvalues[0]);
   Serial.print(", IR_2: ");
-  Serial.print(IRvalue2);
+  Serial.print(IRvalues[1]);
   Serial.print(", IR_3: ");
-  Serial.print(IRvalue3);
+  Serial.print(IRvalues[2]);
   Serial.print(", IR_4: ");
-  Serial.println(IRvalue4);
+  Serial.println(IRvalues[3]);
+
+  return IRvalues;
 }
