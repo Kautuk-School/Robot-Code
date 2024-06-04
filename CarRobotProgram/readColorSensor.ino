fa92cbf8ed9b6e7e480373495033c5b8a751b704
Kautuk-School <169073469+Kautuk-School@users.noreply.github.com>
2024-06-04 11:59:05 -0400

    Update readColorSensor.ino

--- CarRobotProgram/readColorSensor.ino
+++ CarRobotProgram/readColorSensor.ino
@@ -31,47 +31,104 @@ void readColorSensor() {
   Serial.print(" W(clear)= ");
   Serial.println(avgWhite);
 
-// Define variables to store previous LED state
-int prevRed = 0;
-int prevGreen = 0;
-int prevBlue = 0;
-
-if (avgRed > 20 && avgGreen > 15) {
+  // Define variables to store previous LED state
+  int prevRed = 0;
+  int prevGreen = 0;
+  int prevBlue = 0;
+  
+  /*
+  if (avgRed > 20 && avgGreen > 15) {
     // Update only if the color is different from the previous one
     if (prevRed != 0 || prevGreen != 255 || prevBlue != 255) {
-        prevRed = 0;
-        prevGreen = 255;
-        prevBlue = 255;
-        WiFiDrv::analogWrite(27, prevRed);
-        WiFiDrv::analogWrite(25, prevGreen);
-        WiFiDrv::analogWrite(26, prevBlue);
+      prevRed = 0;
+      prevGreen = 255;
+      prevBlue = 255;
+      WiFiDrv::analogWrite(27, prevRed);
+      WiFiDrv::analogWrite(25, prevGreen);
+      WiFiDrv::analogWrite(26, prevBlue);
     }
-} else if (avgRed > 15) {
+  } else if (avgRed > 15) {
     if (prevRed != 255 || prevGreen != 0 || prevBlue != 0) {
-        prevRed = 255;
-        prevGreen = 0;
-        prevBlue = 0;
-        WiFiDrv::analogWrite(26, prevRed);
-        WiFiDrv::analogWrite(25, prevGreen);
-        WiFiDrv::analogWrite(27, prevBlue);
+      prevRed = 255;
+      prevGreen = 0;
+      prevBlue = 0;
+      WiFiDrv::analogWrite(26, prevRed);
+      WiFiDrv::analogWrite(25, prevGreen);
+      WiFiDrv::analogWrite(27, prevBlue);
     }
-} else if (avgGreen > 15) {
+  } else if (avgGreen > 15) {
     if (prevRed != 0 || prevGreen != 255 || prevBlue != 0) {
-        prevRed = 0;
-        prevGreen = 255;
-        prevBlue = 0;
-        WiFiDrv::analogWrite(25, prevGreen);
-        WiFiDrv::analogWrite(26, prevBlue);
-        WiFiDrv::analogWrite(27, prevRed);
+      prevRed = 0;
+      prevGreen = 255;
+      prevBlue = 0;
+      WiFiDrv::analogWrite(25, prevGreen);
+      WiFiDrv::analogWrite(26, prevBlue);
+      WiFiDrv::analogWrite(27, prevRed);
     }
-} else if (avgBlue > 15) {
+  } else if (avgBlue > 15) {
     if (prevRed != 0 || prevGreen != 0 || prevBlue != 255) {
-        prevRed = 0;
-        prevGreen = 0;
-        prevBlue = 255;
-        WiFiDrv::analogWrite(27, prevBlue);
-        WiFiDrv::analogWrite(26, prevGreen);
-        WiFiDrv::analogWrite(25, prevRed);
+      prevRed = 0;
+      prevGreen = 0;
+      prevBlue = 255;
+      WiFiDrv::analogWrite(27, prevBlue);
+      WiFiDrv::analogWrite(26, prevGreen);
+      WiFiDrv::analogWrite(25, prevRed);
     }
-}
+  }
+  */
+
+  
+  
+  
+  
+  
+  
+  
+  
+  if (((29 < avgRed) && (avgRed < 37)) && ((46 < avgGreen) && (avgGreen < 54)) && ((34 < avgBlue) && (avgBlue < 42))) //Green
+  {
+    if (prevRed != 0 || prevGreen != 255 || prevBlue != 0) {
+      prevRed = 0;
+      prevGreen = 255;
+      prevBlue = 0;
+      WiFiDrv::analogWrite(25, prevGreen);
+      WiFiDrv::analogWrite(26, prevBlue);
+      WiFiDrv::analogWrite(27, prevRed);
+      Serial.println("GREEN RN");
+    }
+  }
+  else if (79 < avgRed < 87 && 53 < avgGreen < 61 && 29 < avgBlue < 37) //yellow
+  {
+    if (prevRed != 0 || prevGreen != 255 || prevBlue != 255) {
+      prevRed = 0;
+      prevGreen = 255;
+      prevBlue = 255;
+      WiFiDrv::analogWrite(27, prevRed);
+      WiFiDrv::analogWrite(25, prevGreen);
+      WiFiDrv::analogWrite(26, prevBlue);
+      Serial.println("Yellow RN");
+    }
+  }
+  else if (11 < avgRed < 19 && 19 < avgGreen < 27 && 46 < avgBlue < 54) //blue
+  {
+    if (prevRed != 0 || prevGreen != 0 || prevBlue != 255) {
+      prevRed = 0;
+      prevGreen = 0;
+      prevBlue = 255;
+      WiFiDrv::analogWrite(27, prevBlue);
+      WiFiDrv::analogWrite(26, prevGreen);
+      WiFiDrv::analogWrite(25, prevRed);
+    }
+  } 
+  else if (56 < avgRed < 64 && 11 < avgGreen < 19 && 18 < avgBlue < 26) //red
+  {
+    if (prevRed != 255 || prevGreen != 0 || prevBlue != 0) {
+      prevRed = 255;
+      prevGreen = 0;
+      prevBlue = 0;
+      WiFiDrv::analogWrite(26, prevRed);
+      WiFiDrv::analogWrite(25, prevGreen);
+      WiFiDrv::analogWrite(27, prevBlue);
+    }
+  } 
 }
